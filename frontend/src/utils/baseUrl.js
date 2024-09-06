4ede4275f106d300a42abda2538bc5a530ece3a9
Debash1sh-Panda <debasishpanda2036@gmail.com>
2024-09-06 10:23:00 +0530

    add

--- frontend/src/utils/baseUrl.js
+++ frontend/src/utils/baseUrl.js
@@ -1,4 +1,4 @@
-export const BASE_URL_API_USER = "http://localhost:2008/api/user";
-export const BASE_URL_JOB = "http://localhost:2008/api/job";
-export const BASE_URL_API_COMPANY = "http://localhost:2008/api/company";
-export const BASE_URL_API_APPLICATION = "http://localhost:2008/api/application";
+export const BASE_URL_API_USER = "https://jobhoga.onrender.com/api/user";
+export const BASE_URL_JOB = "https://jobhoga.onrender.com/api/job";
+export const BASE_URL_API_COMPANY = "https://jobhoga.onrender.com/api/company";
+export const BASE_URL_API_APPLICATION = "https://jobhoga.onrender.com/api/application";
