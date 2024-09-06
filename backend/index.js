a45c034b66e47909d52a54a323f99907b0e65c72
Debash1sh-Panda <debasishpanda2036@gmail.com>
2024-09-06 10:11:12 +0530

    add

--- backend/index.js
+++ backend/index.js
@@ -11,7 +11,7 @@ JobApp.use(express.json());
 JobApp.use(express.urlencoded({extended: true}));
 JobApp.use(cookieparser());
 JobApp.use(cors({
-    origin: 'http://localhost:5173',
+    origin: "https://job-hoga-dl452xvey-debash1sh-pandas-projects.vercel.app",
     credentials: true,
   }));
 
